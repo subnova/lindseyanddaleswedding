056aa81444ec6ded1b1bd17483b054ee4a46e5c9
Dale Peakall <dpeakall@thoughtworks.com>
2015-12-18 17:20:40 +0000

    Added countdown clock.

---
+++ js/app/countdown.js
@@ -0,0 +1,14 @@
+define(['jquery', 'flipclock'], function($) {
+    return function(cssSelector) {
+        var currentDate = new Date(),
+            weddingDate  = new Date(2016, 8, 6);
+
+        // Calculate the difference in seconds between the future and current date
+        var diff = weddingDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+        // Instantiate a coutdown FlipClock
+        clock = $(cssSelector).FlipClock(diff, {
+            clockFace: 'DailyCounter'
+        });
+    };
+});
--- js/default.js
+++ js/default.js
@@ -1,6 +1,8 @@
 requirejs(['./config'], function() {
-	requirejs(['js/site', 'app/load-map', 'app/navbar'], function(site, loadMap, navbar) {
+	requirejs(['js/site', 'app/load-map', 'app/navbar', 'app/countdown'], function(site, loadMap, navbar, countdown) {
 		navbar();
+
+        countdown('.countdown-clock');
 
         loadMap.loadMap('#map', site);
 	});
